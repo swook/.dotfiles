83213b5904c1adfe5f839d6a800dba39c23a52a2
Seon-Wook Park <seon.wook@swook.net>
2013-12-31 22:40:39 +0100

    Make godocplay work

--- scripts/godocplay.zsh
+++ scripts/godocplay.zsh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 source $HOME/.zshrc
 
 # Run godoc
 cd $HOME/src/go.tools/cmd/godoc
 go build
-godoc -http=:6060&
+./godoc -http=:6060&
 
 # Run goplay
 cd $GOROOT/misc/goplay/
 go build
-goplay&
+./goplay&
