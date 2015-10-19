06e468a76a6adb89e447cf83008fda63ba57b352
Seon-Wook Park <seon.wook@swook.net>
2015-10-19 08:32:18 +0200

    zsh: alias changes

--- .zsh/plugins/swook/swook.plugin.zsh
+++ .zsh/plugins/swook/swook.plugin.zsh
@@ -7,18 +7,22 @@ alias up='ping 8.8.8.8'
 alias pass='apg -m 15 -x 22 -M lN'
 
 # Debian apt
-alias apt-get='sudo apt-get'
-alias aup='apt-get update'
-alias adist='apt-get dist-upgrade'
-alias aupdist='aup && adist'
-alias arem='apt-get remove --purge'
-alias aarem='apt-get autoremove --purge'
+alias aup='sudo apt-get update'
+alias adist='sudo apt-get dist-upgrade'
+alias asafe='sudo aptitude safe-upgrade'
+alias afull='sudo aptitude full-upgrade'
+alias aupsafe='aup && asafe'
+alias arem='sudo apt-get remove --purge'
+alias aarem='sudo apt-get autoremove --purge'
 alias asearch='aptitude search'
 alias ashow='aptitude show'
-alias ainst='apt-get install'
+alias ainst='sudo apt-get install'
 alias afile='apt-file'
 alias ahold='sudo apt-mark hold'
 alias aunhold='sudo apt-mark unhold'
+
+# KDE
+alias kreset='killall -9 plasmashell && kstart kwin_x11 --replace && kstart plasmashell'
 
 # Network reset
 alias netreset='sudo ifconfig eth0 down && \
